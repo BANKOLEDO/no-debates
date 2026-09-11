f7a0839ca4267aec50f729fd5f554b7c406e0d76
Bankole David <bankydavid12@gmail.com>
2026-09-11 10:56:49 +0100

    feat: provide Convex client when configured

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ConvexProvider } from 'convex/react';
+import { convexClient } from './lib/convexClient';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {convexClient ? (
+      <ConvexProvider client={convexClient}>
+        <App />
+      </ConvexProvider>
+    ) : (
+      <App />
+    )}
   </React.StrictMode>,
 );
